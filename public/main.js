0fdb23a7b9db3e22c68c3e39947c1061a9faca1a
Nicu Parente <nicuparente@gmail.com>
2018-07-22 03:04:19 -0700

    Before dying

--- public/main.js
+++ public/main.js
@@ -5,15 +5,18 @@ $(function () {
   let template = Handlebars.compile(foundationSource);
   let campaignData = { campaigns: [] }
   let campaignLookupArr = [];
+  let companyId=getUrlParams('companyId')
 
-  $.get('http://localhost:3000/api/company/5b53e12ae54e71bcc84c499e')
+  $.get(`http://localhost:3000/api/company/${companyId}`)
     .then((data) => {
       console.log(data);
       let cleanedData = data.foundations.map(foundation => {
         return { name: foundation.foundationName, totalRaised: foundation.foundationContribution }
       })
-
-      campaignArr = data.campaigns;
+      
+      // Set total Contributions
+      $('#TotalContributions').text(`Total Contributions: $${data.totalContributions}`)
+      campaignLookupArr = data.campaigns;
 
       foundationData = { foundations: cleanedData }
       $("#foundationTable").append(template(foundationData));
@@ -22,7 +25,6 @@ $(function () {
         .then(campaigns => {
           campaigns.forEach(c => {
             if (campaignLookupArr.indexOf(c._id)) {
-              console.log(c)
               campaignData.campaigns.push({ name: c.name, status: "Running", totalRaised: c.contributions, targetContribution: c.targetContribution })
             }
           });
@@ -33,7 +35,7 @@ $(function () {
         })
     })
 
-
+  
   var chartData = {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
     datasets: [
@@ -66,3 +68,23 @@ $(function () {
 
 
 });
+
+
+function getUrlParams( prop ) {
+  var params = {};
+  var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
+  var definitions = search.split( '&' );
+
+  definitions.forEach( function( val, key ) {
+      var parts = val.split( '=', 2 );
+      params[ parts[ 0 ] ] = parts[ 1 ];
+  } );
+
+  return ( prop && prop in params ) ? params[ prop ] : params;
+}
+
+function EpochToHourConverter(epochTime){
+  let d = new Date(0);
+  d.setUTCSeconds(epochTime)
+  return `${d.getHours()}:${d.getMinutes()}`
+}
